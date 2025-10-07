01cb06059aa6516d0c8f51ab0221a7ae5f6a5e45
sonyik <dsergey.2006@yandex.ru>
2025-10-07 10:51:06 +0300

    modified:   Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -4,22 +4,16 @@
 std::vector<std::vector<char>> CreateField(int width, int height)
 {
     std::vector<std::vector<char>> field(height, std::vector<char>(width, ' '));
-    
-    for (int i = 0; i < height; ++i)
+    for (int j = 0; j < width; j++) 
     {
-        for (int j = 0; j < width; ++j)
-        {
-            if (i == 0 || i == height - 1)
-            {
-                field[i][j] = '@';  
-            }
-            else if (j == 0 || j == width - 1)
-            {
-                field[i][j] = '@'; 
-            }
-        }
+        field[0][j] = '@';
+        field[height-1][j] = '@';
     }
-    
+    for (int i = 1; i < height-1; i++) 
+    {
+        field[i][0] = '@';
+        field[i][width-1] = '@';
+    }
     return field;
 }
 
