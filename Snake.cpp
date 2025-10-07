bc977ef9f1ca5d8deb8be3be63f35c623def7bb4
sonyik <dsergey.2006@yandex.ru>
2025-10-07 10:42:14 +0300

    modified:   Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <vector>
 
-main()
+std::vector<std::vector<char>> CreateField(int width, int height)
 {
-    std::cout << "Test" << std::endl;
+    std::vector<std::vector<char>> field(height, std::vector<char>(width, ' '));
+    
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            if (i == 0 || i == height - 1)
+            {
+                field[i][j] = '@';  
+            }
+            else if (j == 0 || j == width - 1)
+            {
+                field[i][j] = '@'; 
+            }
+        }
+    }
+    
+    return field;
+}
+
+void PrintField(const std::vector<std::vector<char>>& field)
+{
+    for (int i = 0; i < field.size(); ++i)
+    {
+        for (int j = 0; j < field[i].size(); ++j)
+        {
+            std::cout << field[i][j];
+        }
+        std::cout << '\n';
+    }
+}
+
+int main()
+{
+    const int FIELD_WIDTH = 20;
+    const int FIELD_HEIGHT = 30;
+    std::vector<std::vector<char>> gameField = CreateField(FIELD_WIDTH, FIELD_HEIGHT);
+    PrintField(gameField);
     return 0;
 }
